705fd25cf25eeb89fec3781084053cf3e1d8ceb1
Antonio Hu <10717798@polimi.it>
2024-03-11 15:27:36 +0100

    Fixed the issue about images' size

--- src/components/main/ChatMessages.jsx
+++ src/components/main/ChatMessages.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import './mainStyle/ChatMessageStyle.css'
 
 export default function ChatMessages({ messages }) {
   return (
     <div className="chat-messages">
       {messages.map((message, index) => (
         <div key={index} className="message">
+            <div className="imageContainer">
             <img src={message.userProfileImage} alt={`${message.user}'s profile`} className="profile-image" />
+            </div>
           <strong>{message.user}:</strong> 
           <span>{message.message}</span>
           <span className="timestamp">{message.time}</span>
